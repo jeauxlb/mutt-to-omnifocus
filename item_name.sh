bd663a7b5dc5170da4dd2a254df7ed3154c48b11
Joel Buckley <github@spam.joelbuckley.com.au>
2017-04-09 22:12:24 +1000

    Initial commit

---
+++ item_name.sh
@@ -0,0 +1,10 @@
+#!/usr/local/Cellar/bash/4.3.46/bin/bash
+
+RESET="\033[0m"
+BOLD="\033[1m"
+RED="\033[31m"
+#RED=""
+MUTT="Mutt: $1"
+read -e -p "$(echo -e $BOLD$RED"Task Name: "$RESET$RESET$RESET$RESET$RESET$RESET$RESET$RESET$RESET)" -i "$MUTT" title </dev/tty
+
+echo "$title"
